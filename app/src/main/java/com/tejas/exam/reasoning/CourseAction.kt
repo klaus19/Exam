2258642b91ee218ee5e1daf68129b72ad710c7aa
Tejas <tejaskhartude@gmail.com>
2021-09-25 16:14:01 +0530

    activity_course_of_action.xml and Courseof Action.kt updated

--- app/src/main/java/com/tejas/exam/reasoning/CourseAction.kt
+++ app/src/main/java/com/tejas/exam/reasoning/CourseAction.kt
@@ -56,8 +56,48 @@ class CourseAction: AppCompatActivity() {
     lateinit var txtViewAnswer10CourseofAction: TextView
     @BindView(R.id.textAnswer10CourseofAction)
     lateinit var textAnswer10CourseofAction: TextView
+    @BindView(R.id.textAnswer11CourseofAction)
+    lateinit var textAnswer11CourseofAction: TextView
+    @BindView(R.id.textAnswer12CourseofAction)
+    lateinit var textAnswer12CourseofAction: TextView
+    @BindView(R.id.textAnswer13CourseofAction)
+    lateinit var textAnswer13CourseofAction: TextView
+    @BindView(R.id.textAnswer14CourseofAction)
+    lateinit var textAnswer14CourseofAction: TextView
+    @BindView(R.id.textAnswer15CourseofAction)
+    lateinit var textAnswer15CourseofAction: TextView
+    @BindView(R.id.textAnswer16CourseofAction)
+    lateinit var textAnswer16CourseofAction: TextView
+    @BindView(R.id.textAnswer17CourseofAction)
+    lateinit var textAnswer17CourseofAction: TextView
+    @BindView(R.id.textAnswer18CourseofAction)
+    lateinit var textAnswer18CourseofAction: TextView
+    @BindView(R.id.textAnswer19CourseofAction)
+    lateinit var textAnswer19CourseofAction: TextView
+    @BindView(R.id.textAnswer20CourseofAction)
+    lateinit var textAnswer20CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer11CourseofAction)
+    lateinit var txtViewAnswer11CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer12CourseofAction)
+    lateinit var txtViewAnswer12CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer13CourseofAction)
+    lateinit var txtViewAnswer13CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer14CourseofAction)
+    lateinit var txtViewAnswer14CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer15CourseofAction)
+    lateinit var txtViewAnswer15CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer16CourseofAction)
+    lateinit var txtViewAnswer16CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer17CourseofAction)
+    lateinit var txtViewAnswer17CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer18CourseofAction)
+    lateinit var txtViewAnswer18CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer19CourseofAction)
+    lateinit var txtViewAnswer19CourseofAction: TextView
+    @BindView(R.id.txtViewAnswer20CourseofAction)
+    lateinit var txtViewAnswer20CourseofAction: TextView
+    
 //    RadioGroup Binding
-
     @BindView(R.id.radioGroup1CourseofAction)
     lateinit var radioGroup1CourseofAction: RadioGroup
     @BindView(R.id.radioGroup2CourseofAction)
@@ -78,6 +118,27 @@ class CourseAction: AppCompatActivity() {
     lateinit var radioGroup9CourseofAction: RadioGroup
     @BindView(R.id.radioGroup10CourseofAction)
     lateinit var radioGroup10CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup11CourseofAction)
+    lateinit var radioGroup11CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup12CourseofAction)
+    lateinit var radioGroup12CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup13CourseofAction)
+    lateinit var radioGroup13CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup14CourseofAction)
+    lateinit var radioGroup14CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup15CourseofAction)
+    lateinit var radioGroup15CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup16CourseofAction)
+    lateinit var radioGroup16CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup17CourseofAction)
+    lateinit var radioGroup17CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup18CourseofAction)
+    lateinit var radioGroup18CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup19CourseofAction)
+    lateinit var radioGroup19CourseofAction: RadioGroup
+    @BindView(R.id.radioGroup20CourseofAction)
+    lateinit var radioGroup20CourseofAction: RadioGroup
+            
 
 
 
@@ -130,6 +191,180 @@ class CourseAction: AppCompatActivity() {
             textAnswer10CourseofAction.visibility = if (textAnswer10CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
         }
 
+        txtViewAnswer11CourseofAction.setOnClickListener {
+            textAnswer11CourseofAction.visibility = if (textAnswer11CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer12CourseofAction.setOnClickListener {
+            textAnswer12CourseofAction.visibility = if (textAnswer12CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer13CourseofAction.setOnClickListener {
+            textAnswer13CourseofAction.visibility = if (textAnswer13CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer14CourseofAction.setOnClickListener {
+            textAnswer14CourseofAction.visibility = if (textAnswer14CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer15CourseofAction.setOnClickListener {
+            textAnswer15CourseofAction.visibility = if (textAnswer15CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer16CourseofAction.setOnClickListener {
+            textAnswer16CourseofAction.visibility = if (textAnswer16CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer17CourseofAction.setOnClickListener {
+            textAnswer17CourseofAction.visibility = if (textAnswer17CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer18CourseofAction.setOnClickListener {
+            textAnswer18CourseofAction.visibility = if (textAnswer18CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer19CourseofAction.setOnClickListener {
+            textAnswer19CourseofAction.visibility = if (textAnswer19CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+        txtViewAnswer20CourseofAction.setOnClickListener {
+            textAnswer20CourseofAction.visibility = if (textAnswer20CourseofAction.visibility == View.INVISIBLE) View.VISIBLE else View.INVISIBLE
+        }
+
+        radioGroup11CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId==R.id.radioButton42CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+
+        radioGroup12CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId==R.id.radioButton48CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+        radioGroup13CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId == R.id.radioButton50CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+
+        radioGroup14CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId==R.id.radioButton53CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+        radioGroup15CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId == R.id.radioButton59CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+        radioGroup16CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId==R.id.radioButton62CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+        radioGroup17CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId==R.id.radioButton66CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+        radioGroup18CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId ==R.id.radioButton69CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+        radioGroup19CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId==R.id.radioButton73CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+        radioGroup20CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+
+
+            if (checkedId==R.id.radioButton78CourseofAction) {
+                Snackbar.make(findViewById(android.R.id.content), "Correct", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(0, 128, 0))
+                    .show()
+
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), "InCorrect", Snackbar.LENGTH_LONG)
+                    .withColor(Color.rgb(255, 0, 0))
+                    .show()
+            }
+        })
+        
         radioGroup1CourseofAction.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener{ group, checkedId ->
 
            
