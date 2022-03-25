ee6fe81c63edaecb7f24c047b788c4f1eff6f9e3
Tejas <tejaskhartude@gmail.com>
2022-03-25 21:57:12 +0530

    Test1.kt got added new functionality about customised AlertDialog

--- app/src/main/java/com/tejas/exam/onlinetests/Test1.kt
+++ app/src/main/java/com/tejas/exam/onlinetests/Test1.kt
@@ -1,19 +1,71 @@
 package com.tejas.exam.onlinetests
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.LinearLayout
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.tejas.exam.databinding.ActivityTest1Binding
 
 class Test1:AppCompatActivity() {
 
     private lateinit var binding:ActivityTest1Binding
+    var counter=0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTest1Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        startwithTimer()
 
-
-
+        binding.btnTest1Submit.setOnClickListener {
+              countMarks()
+        }
     }
+    fun startwithTimer(){
+        object : CountDownTimer(150000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                binding.timer1.text = counter.toString()
+                counter++
+            }
+
+            override fun onFinish() {
+                binding.timer1.text = "Finished"
+            }
+
+        }.start()
+    }
+
+
+    //programmatically creating a Alert Dialog in Kotlin
+
+        fun countMarks(){
+
+            val builder = AlertDialog.Builder(this)
+            val linear = LinearLayout(this)
+
+            //Set a Title
+            builder.setTitle("Final Score")
+
+            //Set a Message
+            builder.setMessage("You Final score is $counter")
+
+            linear.orientation = LinearLayout.VERTICAL
+            builder.setView(linear)
+
+            //set a Positive Button
+            builder.setPositiveButton(
+                "Ok"){builder, id ->
+                //Go back to OnlineTests.kt
+                startActivity(Intent(this@Test1,OnlineTests::class.java))
+            }
+
+            builder.show()
+
+
+        }
+
 }
--- app/src/main/java/com/tejas/exam/onlinetests/Test2.kt
+++ app/src/main/java/com/tejas/exam/onlinetests/Test2.kt
@@ -1,8 +1,22 @@
 package com.tejas.exam.onlinetests
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.tejas.exam.databinding.ActivityTest2Binding
 
 class Test2:AppCompatActivity() {
 
+   private lateinit var binding:ActivityTest2Binding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTest2Binding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(binding.root)
+        }
+
+
+
+
+    }
 }
